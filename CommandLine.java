17bfd2cdfcf7f67591fa6168cda2c2e932895704
SCORPION <54326987+SCORP1ON-AI@users.noreply.github.com>
2020-12-05 15:59:51 +0530

    Demonstrate Comman Line Argument

--- CommandLine.java
+++ CommandLine.java
@@ -1,3 +1,12 @@
+/*
+    Demonstrate Commmand line Argument
+    >> javac CommandLine.java
+    >> java CommandLine This is argument
+    >> args[0]: This
+       args[1]: is
+       args[2]: test
+*/
+
 public class CommandLine {
     public static void main(String args[]){
         for(int i = 0; i < args.length; i++){
